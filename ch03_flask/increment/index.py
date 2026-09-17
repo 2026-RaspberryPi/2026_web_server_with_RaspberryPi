2ccf6b94e3e6b98ae1954f454b915fea9cde2e01
bssmMrchoi <93627909+bssmMrchoi@users.noreply.github.com>
2026-09-17 12:04:36 +0900

    Implement save number functionality with GET and POST
    Added functionality to save numbers via GET and POST requests, including database insertion logic.

--- ch03_flask/increment/index.py
+++ ch03_flask/increment/index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+import pymysql
 
 app = Flask(__name__)
 
@@ -6,11 +7,35 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/숫자") 
-def 디비에 저장하는 함수():
-    디비 불러서 값 저장시키기
-    return / 주소 다시 부름
 
+@app.route("/save/<num>")
+def save_num_get(num):
+    '''
+        1. numcount 테이블 생성(id, num, insert_at)
+        2. pymysql로 연결
+        3. 증가 한 수만큼 추가
+        4. 연결끊기
+        5. db 접속해서 조회
+    '''
+    conn = pymysql.connect(host='localhost', user='bssmMrchoi', password='q1w2e3', db='study')
+    cur = conn.cursor()
+    cur.execute("insert into numcount(num) values({0})".format(num))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("index"))
+    
+
+@app.route("/submit", methods=["POST"])
+def save_num_post():
+    data = request.get_json()
+    print(data.get("value"))
+    conn = pymysql.connect(host='localhost', user='root', password='q1w2e3', db='study')
+    cur = conn.cursor()
+    cur.execute(f"insert into numcount(num) values({data.get("value")})")
+    conn.commit()
+    conn.close()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
