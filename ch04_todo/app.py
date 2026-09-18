1d7e9bd6942d281555072a98b529da8dbf22c702
bssmMrchoi <teacher009@bssm.hs.kr>
2026-09-18 11:39:04 +0900

    modify: 투두리스트 최종 정리

--- ch04_todo/app.py
+++ ch04_todo/app.py
@@ -3,35 +3,33 @@ from model.todo_db import TodoDB
 
 app = Flask(__name__)
 todo_db = TodoDB()
-cache_list = []
 
 @app.route("/")
 def home():
-    global cache_list
     tasks = todo_db.get()
     print(tasks)
-    cache_list = tasks
-    print(cache_list)
     return render_template("task.html", tasks=tasks)
+
 
 @app.route('/add', methods=['POST'])
 def add_task():
     title = request.form['title']
     todo_db.add(title)
-    return redirect('/')
+    return redirect("/")
 
-@app.route('/complete/<int:index>')
-def complete_task(index):
-    print(f'index : {index}')
-    todo_index = cache_list[index][0]
+
+@app.route('/complete/<int:todo_index>', methods=['PATCH'])
+def complete_task(todo_index):
+    print(f'index : {todo_index}')
     todo_db.completed(todo_index)
-    return redirect('/')
+    return "", 200
 
-@app.route('/delete/<int:index>')
-def delete_task(index):
-    todo_index = cache_list[index][0]
+
+@app.route('/delete/<int:todo_index>', methods=['DELETE'])
+def delete_task(todo_index):
     todo_db.remove(todo_index)
-    return redirect('/')
+    return "", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
